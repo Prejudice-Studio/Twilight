9382d50e21d4dd06075886b2631ca0d3566ce4c2
Touko Nanami <MoYuanCN@gmail.com>
2025-11-27 16:02:05 +0800

    Add new config options for email and BGM modes
    Introduced GLOBAL_BGM_MODE, EMAIL_BIND, and FORCE_BIND_EMAIL options to the Config class for enhanced feature toggling. Updated comments for user notification settings and added SCORE_NAME to ScoreAndRegisterConfig for customizable point naming.

--- src/config.py
+++ src/config.py
@@ -57,7 +57,10 @@ class Config(BaseConfig):
     MAX_RETRY: int = 3  # 重试次数
     DATABASES_DIR: Path = ROOT_PATH / 'db'  # 数据库路径
     BANGUMI_TOKEN: str = ''  # Bangumi Token
+    GLOBAL_BGM_MODE: bool = False  # 是否允许BGM点格子
     TELEGRAM_MODE: bool = False  # 是否开启Telegram模式
+    EMAIL_BIND: bool = False  # 是否绑定邮箱
+    FORCE_BIND_EMAIL: bool = False  # 是否强制绑定邮箱
     FORCE_BIND_TELEGRAM: bool = True  # 是否强制绑定Telegram
     
 class EmbyConfig(BaseConfig):
@@ -83,16 +86,16 @@ class ScoreAndRegisterConfig(BaseConfig):
     """
     积分及注册配置管理类。
     """
+    SCORE_NAME: str = '暮光币'  # 积分名称
     REGISTER_MODE: bool = False  # 是否允许注册功能
     REGISTER_CODE_LIMIT: bool = False  # REGISTER_MODE为True时，是否允许注册码注册
     SCORE_REGISTER_MODE: bool = False  # 是否允许积分注册
     SCORE_REGISTER_NEED: int = 100  # SCORE_REGISTER_MODE为True时，注册所需积分
     USER_LIMIT: int = 200 # 允许的已注册用户数量上限
-    NEW_USER_NOTICE_STATUS: bool = False  # 新用户通知开关
-    NEW_USER_NOTICE_LINK: bool = False  # 新用户通知是否指向个人简介
+    NEW_USER_NOTICE_STATUS: bool = False  # 用户注册/续期/白名单通知开关
+    NEW_USER_NOTICE_LINK: bool = False  # 用户注册/续期/白名单通知是否指向个人主页通知是否指向个人简介
     RED_PACKET_MODE: bool = False  # 是否开启红包功能
-    PRIVATE_TRANSFER_MODE: bool = False # 私人转账
-    
+    PRIVATE_TRANSFER_MODE: bool = False # 是否开启私人转账
 
 Config.update_from_toml("Global")
 EmbyConfig.update_from_toml('Emby')
