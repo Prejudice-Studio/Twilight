30e095dc4db0fe20e20a3e5679b1332f1d1e4c90
Touko Nanami <MoYuanCN@gmail.com>
2026-04-22 08:22:57 +0800

    Clear

--- webui/src/hooks/use-region-refresh.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-import { subscribeRegionRefresh } from "@/lib/region-refresh";
-
-export function useRegionRefresh(region: string, handler: (payload?: unknown) => void) {
-  useEffect(() => {
-    return subscribeRegionRefresh(region, handler);
-  }, [region, handler]);
-}
